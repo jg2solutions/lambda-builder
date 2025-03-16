a0ba1ae309c401dfe174ac9943a98a0c9324028e
Joel Garcia Jr <joel.garciajr84@gmail.com>
2025-03-15 22:03:59 -0300

    Update Lambda response message to include 'xxx'

--- lambda/main.py
+++ lambda/main.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     print("Lambda executada com sucesso!", event)
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Hello from Lambda!"})
+        "body": json.dumps({"message": "Hello from Lambda!xxx"})
     }
